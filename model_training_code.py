6394ba4273013f3491d16f3d15cedc80c8343686
skatsushi <skatsushi2025@gmail.com>
2024-07-23 15:11:05 +0900

    Update model_training_code.py

--- model_training_code.py
+++ model_training_code.py
@@ -7,6 +7,7 @@ from google.cloud import storage
 from joblib import dump
 from sklearn.pipeline import make_pipeline
 
+
 storage_client = storage.Client()
 bucket = storage_client.bucket("sid-ml-ops-polyinfo")
 
